0e4e6d8e45287a9d63ca58c91e36658e0a3c2de1
Ichiyou1922 <mogi2fruits.kazu@gmail.com>
2025-11-29 02:45:57 +0900

    add new codes

---
+++ stack-LIFO/stack.c
@@ -0,0 +1,64 @@
+/*
+push:
+1. mallocでノードを作る
+->関数の中で宣言された変数は関数が終わるとメモリから消去される．
+->main関数に戻った時topが指すのはメモリ解放後の跡地．
+->関数が終わっても消えないメモリがヒープ領域でありmalloc
+Node *new_node = (Node *)malloc(sizeof(Node));では何をしている？
+->sizeof(Node)で必要なメモリサイズを計算する．
+->malloc(...)でヒープ領域からメモリを確保するよう要求．freeされるまで解放されない．
+->mallocで返されるのはvoid * だからこれを(Node *)でキャスト(型変換)する．
+2. 新ノードのnextに現在のトップ(*top)を入れる
+3. (*top)を新ノードに書き換える
+
+pop:
+1. 空チェック(*top == NULL)
+2. 現在のトップ(*top)を一時変数tempに保存
+3. データを取り出す(int val = temp->data)
+4. トップ(*top)をtemp->nextに進める
+5. free(temp)する
+6. データを返す
+
+なぜポインタのポインタ？
+関数の戻り値がintとして埋まっているため，関数内で変数の変更を行いたいから．
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+bool push(Node **top, int data) {
+  Node *new_node = (Node *)malloc(sizeof(Node));
+  if (new_node == NULL) {
+    printf("Something is wrong...\n");
+    return false;
+  }
+  new_node->next = *top;
+  new_node = *top;
+  return true;
+}
+
+int pop(Node **top) {
+  if (*top == NULL) {
+    printf("Stack is empty...\n");
+    return 1;
+  }
+  Node *temp = *top;
+  int val = temp->data;
+  *top = temp->next;
+  free(temp);
+  return (val);
+}
+
+int peek(Node **top) {
+  if (*top == NULL) {
+    printf("Not exist the value...\n");
+    return -1;
+  }
+  printf("Top value is %s", *top->data);
+  return 0;
+}
+
+void free_stack(Node **top) {
+  free(*top);
+}
